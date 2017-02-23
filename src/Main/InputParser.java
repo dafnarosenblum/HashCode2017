9d6fda912e7bfbba3d21a9e5c5a0326051e374c2
dafnar <dafi.moin@gmail.com>
2017-02-23 22:21:49 +0200

    added functionality to parser

--- src/Main/InputParser.java
+++ src/Main/InputParser.java
@@ -7,17 +7,41 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import Entities.CacheServer;
+import Entities.EndPoint;
 import Entities.Video;
 
 public class InputParser {
 
-	public static void parse(String fileName) throws IOException {
+	public static void parse(String fileName, List<Video> videos, List<EndPoint> endPoints,
+			List<CacheServer> cacheServers) throws IOException {
 		Iterator<String> lines = Files.lines(Paths.get(fileName)).iterator();
 		String[] firstLine = lines.next().split(" ");
 		String[] secondLine = lines.next().split(" ");
+		int numberOfVideos = Integer.valueOf(firstLine[0]);
+		int numberOfEndPoints = Integer.valueOf(firstLine[1]);
+		videos = parseVideos(secondLine, numberOfVideos);
+		endPoints = parseEndPoints(lines, numberOfEndPoints);
+	}
+
+	private static List<EndPoint> parseEndPoints(Iterator<String> lines, int numberOfEndPoints) {
+		List<EndPoint> endPoints = new ArrayList<>();
+		int endPointsCounter = 0;
+		while (endPointsCounter < numberOfEndPoints) {
+			String[] dataCenterLatencyAndNumOfCaches = lines.next().split(" ");
+			long dataCenterLatency = Long.valueOf(dataCenterLatencyAndNumOfCaches[0]);
+			int numOfCaches = Integer.valueOf(dataCenterLatencyAndNumOfCaches[1]);
+			EndPoint endPoint = new EndPoint(endPointsCounter, dataCenterLatency);
+			endPointsCounter++;
+		}
+		return endPoints;
+	}
+
+	private static List<Video> parseVideos(String[] secondLine, int numberOfVideos) {
 		List<Video> videos = new ArrayList<>();
-		for (int id = 0; id < Integer.valueOf(firstLine[0]); id++) {
+		for (int id = 0; id < numberOfVideos; id++) {
 			videos.add(new Video(Integer.valueOf(id), Integer.valueOf(secondLine[id])));
 		}
+		return videos;
 	}
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,31 +1,34 @@
 package Main;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import Entities.CacheServer;
+import Entities.EndPoint;
 import Entities.Video;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// parser work
-	    
+		List<EndPoint> endPoints = new ArrayList<>();
+		List<Video> videos = new ArrayList<>();
 		List<CacheServer> cacheServers = new ArrayList<>();
-		
+		InputParser.parse(args[0], videos, endPoints, cacheServers);
+
 		// populate the videos list
 		List<Video> allVideos = new ArrayList<>();
-		
+
 		// for each video, calculate a list of times saved in each cache server
-		
-		
+
 		for (CacheServer cacheServer : cacheServers) {
-		    List <Video> videosForServer = VideoAllocator.pickVideosForCacheServer(cacheServer, allVideos);
-		    cacheServer.setSavedVideos(videosForServer);
-		    // write result to file
+			List<Video> videosForServer = VideoAllocator.pickVideosForCacheServer(cacheServer, allVideos);
+			cacheServer.setSavedVideos(videosForServer);
+			// write result to file
 		}
-		
+
 	}
 
 }
