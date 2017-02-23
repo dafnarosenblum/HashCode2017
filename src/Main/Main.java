a908b15a96656d61ddf988b12eef51c1158d6f2b
ShifShif <shifrb@gmail.com>
2017-02-23 21:43:00 +0200

    reorganized algorithm to be in a separate class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,36 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Entities.CacheServer;
+import Entities.Video;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		// parser work
+	    
 		List<CacheServer> cacheServers = new ArrayList<>();
-
-		// calculate the capacity of the biggest cache server
-		long largestCacheSize = 0;
-		for (CacheServer cs : cacheServers) {
-			if (cs.getSizeMB() > largestCacheSize) {
-				largestCacheSize = cs.getSizeMB();
-			}
+		List<Video> allVideos = new ArrayList<>();
+		
+		for (CacheServer cacheServer : cacheServers) {
+		    List <Video> videosForServer = VideoAllocator.pickVideosForCacheServer(cacheServer, allVideos);
+		    cacheServer.setSavedVideos(videosForServer);
+		    // write result to file
 		}
-		// make a list of all the videos to consider for allocation (all of the videos at this stage)
-
-		// iterate over the list and remove every video that is too big for any cache server
-
-		// iterate over the list and remove every video that has no requests
-
-		// calculate for each video a vector of requests * time_saved for each cache server
-
-		// for each cache server:
-		// calculate the sum of time saved
-		// calculate the constraint
-		// solve for maximum time saved
-
-		// the result is the list of videos to be stored at this cache server
-
+		
 	}
 
 }
